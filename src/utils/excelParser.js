ffca88eb6530a9c6b8409ec487818d465d53276c
lobbygodv1 <31457960+lobbygodv1@users.noreply.github.com>
2025-11-25 22:47:38 -0500

    Create excelParser.js

---
+++ src/utils/excelParser.js
@@ -0,0 +1,17 @@
+import * as XLSX from 'xlsx';
+
+export function parseExcel(file) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      const data = new Uint8Array(e.target.result);
+      const workbook = XLSX.read(data, { type: 'array' });
+      const sheetName = workbook.SheetNames[0];
+      const worksheet = workbook.Sheets[sheetName];
+      const jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
+      resolve(jsonData);
+    };
+    reader.onerror = reject;
+    reader.readAsArrayBuffer(file);
+  });
+}
